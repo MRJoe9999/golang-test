7029f8a8169ee4fc30e8f231fb801c9ba298f4a0
jose <2023159140@ub.edu.bz>
2025-04-03 19:47:33 -0600

    Added the scanning process

--- main.go
+++ main.go
@@ -146,7 +146,27 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net"
+	"strconv"
+	"sync"
+	"time"
 )
+
+func labours(wg *sync.WaitGroup, task chan string, dialer net.Dialer) {
+
+	defer wg.Done()
+	for addr := range task {
+
+		conn, err := dialer.Dial("tcp", addr)
+		if err == nil {
+			fmt.Printf("Connection to %s was successful\n", addr)
+			conn.Close()
+		} else {
+			// Failed connection, print an error message
+			fmt.Printf("Failed to connect to %s: %v\n", addr, err)
+		}
+	}
+}
 
 func main() {
 	target := flag.String("target", "localhost", "Target IP or hostname")
@@ -161,4 +181,29 @@ func main() {
 	fmt.Println("Scanning target:", *target)
 	fmt.Printf("Scanning ports from %d to %d\n", *startPort, *endPort)
 	fmt.Printf("Using %d concurrent workers\n", *workers)
+
+	task := make(chan string, *workers)
+
+	var wg sync.WaitGroup
+
+	dialer := net.Dialer{
+		Timeout: 5 * time.Second,
+	}
+
+	for i := 0; i < *workers; i++ {
+		wg.Add(1)
+		go labours(&wg, task, dialer)
+	}
+
+	for j := *startPort; j <= *endPort; j++ {
+		port := strconv.Itoa(j)
+		address := net.JoinHostPort(*target, port)
+		task <- address
+	}
+
+	close(task)
+	wg.Wait()
+
+	fmt.Println("Scan complete!")
+
 }
